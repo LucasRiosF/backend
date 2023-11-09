2997c21476c6c8391d9e41eb8a5aa6020b7c9d3b
Lucas Rios <lucas.rios@aluno.senai.br>
2023-11-09 17:05:19 -0300

    feat: testes

--- src/server.js
+++ src/server.js
@@ -9,8 +9,39 @@ const app = express(); //cria o servidor
 app.use(express.json()); //permite que o servidor entenda aquisiçoes em JSON
 
 app.get("/", (req, res) => {
+    const nome = req.query.nome;
+    const qualquerNome = req.query.anime;
+    const page = req.query.page;
+    const limt = req.query.limt
     //Rota inicial
-    res.status(200).json({ message: "Hello, World!"})
+    res.status(200).json({ nome: nome, anime: qualquerNome, pagina: page, limite: limt})
+});
+
+app.get("/:nome/:sobrenome/:idade", (req, res) => {
+    const nome = req.params.nome;
+    const sobrenome = req.params.sobrenome
+    const idade = req.params.idade
+    //Rota inicial
+    res.status(200).json({ mensagem: `Meu nome é ${nome} ${sobrenome}, tenho ${idade} anos `})
+});
+
+app.post("/", (req, res) => {
+    const nome = req.body.nome;
+    const sobrenome = req.body.sobrenome
+    const idade = req.body.idade
+    res.status(200).json({ 
+        nomeUsuario: nome,
+        sobrenoUsuario: sobrenome,
+        idadeUsuario: idade  })
+});
+
+
+app.delete("/", (req, res) => {
+    res.status(200).json({ message: "Hello, Delete!"})
+});
+
+app.put("/", (req, res) => {
+    res.status(200).json({ message: "Hello, Put!"})
 });
 
 app.listen(port, () => {
